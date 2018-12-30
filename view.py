e06859e102900c51c68a74c8febff4f07e69ee39
Unknown <oleggordiichenko@gmail.com>
2018-12-30 14:30:12 +0200

    minor

--- view.py
+++ view.py
@@ -5,11 +5,16 @@ from flask import render_template
 # def hello():
 # 	return "Hello World!"
 @app.route('/')
-def h_main():
-	return 'Main, page!'
+def mainP():
+	return render_template('main.html')
+	# return 'Main, page!'
 
 @app.route('/machine/')
-@app.route('/machie/<name>')
-def machine(name=None):  #name=None
-	# name = "name"
+@app.route('/machine/<name>')
+def machineP(name=None):  #name=None
 	return render_template('machine.html', name=name)  # , name=name , name="name"
+
+@app.route('/task/')
+@app.route('/task/<name>')
+def taskP(name=None):  #name=None
+	return render_template('task.html', name=name)  # , name=name , name="name"
