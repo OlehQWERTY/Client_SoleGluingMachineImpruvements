0fce3ba3575c302308b6d39175d642a2dfd2d04c
Unknown <oleggordiichenko@gmail.com>
2019-01-11 15:18:51 +0200

    register form is working now

--- controlWebPage/forms.py
+++ controlWebPage/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+from controlWebPage.modules import User
+
 
 # class RegistrationForm(FlaskForm)
 # 	taskName = StringField('validationTooltip02', validators=[DataRequired(), Length(min=2, max=30)]  # add task завдання
@@ -19,9 +21,24 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('This email is already taken. Please choose another one.')
+    def validate_email(self, email):   
+        email = User.query.filter_by(email=email.data).first()
+        if email:
+            raise ValidationError('This email is already taken. Please choose another one.')
+
+    def validate_password(self, password):
+        password = password.data
+        if len(password) < 6:
+            raise ValidationError('This password is too short. Please choose another one with minimal length equal to 6 signs.')
+
+
 class LoginForm(FlaskForm):
 	email = StringField('Email',
-	                    validators=[DataRequired(), Email()])
+	            validators=[DataRequired(), Email()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	remember = BooleanField('Remember Me')
 	submit = SubmitField('Login')
