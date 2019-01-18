cf5ee523cb5f034001952f99b0004bb1c28a2639
Unknown <oleggordiichenko@gmail.com>
2019-01-18 11:30:57 +0200

    alchemy replaced with mysql.connector

--- controlWebPage/__init__.py
+++ controlWebPage/__init__.py
@@ -6,14 +6,43 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
+import mysql.connector
+
 # some_engine = create_engine('mysql+mysqldb://monitor:password@172.16.2.142:3306/sole_1')
+# python -m pip install mysql-connector
+
+
+mydb = mysql.connector.connect(
+  host="172.16.2.142",
+  user="monitor",
+  passwd="password",
+  database="sole_1"
+)
+
+# print(mydb)
+
+mycursor = mydb.cursor()
+
+# mycursor.execute("SHOW DATABASES")
+# print(mycursor)
+# query = ("SELECT UnitID, Articul, ProcessID, OperatorName, OperationDate, Pull, OrderNumber, LocalNumber, ReadyDate FROM glueMachine")
+# mycursor.execute(query)
+mycursor.execute("SELECT UnitID, Articul, ProcessID, OperatorName, OperationDate, Pull, OrderNumber, LocalNumber, ReadyDate FROM glueMachine")
+
+# mycursor.execute("USE sole_1")
+# mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+  print(x)
+
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' # db controlWebPage
 app.config['SQLALCHEMY_BINDS'] = {'two' : 'sqlite:///two.db',
-                                  'three' : 'sqlite:///three.db'}
+                                  'three' : 'sqlite:///three.db'}#,
+                                  # 'machine': 'mysql+mysqldb://monitor:password@172.16.2.142:3306/sole_1/glueMachine'}
 db = SQLAlchemy(app)
 session_three = db.scoped_session(db.sessionmaker(bind=db.get_engine(app, 'three')))
 bcrypt = Bcrypt(app)
