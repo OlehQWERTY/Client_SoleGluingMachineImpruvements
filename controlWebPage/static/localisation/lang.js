9f5855c6fbd523caa7c04478375b9e72887220e5
Unknown <oleggordiichenko@gmail.com>
2019-01-28 09:59:20 +0200

    translate all and increase accurate js

--- controlWebPage/static/localisation/lang.js
+++ controlWebPage/static/localisation/lang.js
@@ -2,21 +2,28 @@
 // load this after all script cause it raplase names and some functions wouldn't work
 
 console.log("I am with you");
+
+function replaceAll(str, find, replace) {
+	return str.replace(new RegExp(find, 'g'), replace);
+}
+
 function translate(strJson){
 	// window.onload = function () {  // after page is loading
 			// $.getJSON("lang_db.json", function(d){
 			$.getJSON(strJson, function(d){  // read json obj with translation
 				let html = $('body').html();
+
 				for(let i=0; i<d.en.length; i++){  // replase text english to ukranian one
-					html = html.replace(d.en[i], d.ua[i]);
+					// var re = new RegExp("{[(" + d.ua[i] + ")]}", 'g'); // reg expression all (DO NOT WORK: WHY???)
+					var ammount = html.split("{[(" + d.en[i] + ")]}").length;  // ammount of similar strings 
+					console.log(ammount, d.en[i]);
+					for(var n=0;n<=ammount;n++){  // teaplase all similar
+						html = html.replace("{[(" + d.en[i] + ")]}", d.ua[i]);  // replase string with translated string
+						// console.log(n);
+					}
 				}
 				$('body').html(html);
 			})
 
   // }
 }
-
-// var str = "Mr Blue has a blue house and a blue car";
-// var res = str.replace(/blue|house|car/gi, function (x) {
-//   return x.toUpperCase();
-// });
